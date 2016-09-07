c8ceb9be9dd205d3f64413e18734729f1cb6dc38
graciomambo <gracio.mambo@gmail.com>
2016-09-07 08:45:58 +0200

    Inicio

---
+++ Gamer/Gamer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gamer.Startup))]
+namespace Gamer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
